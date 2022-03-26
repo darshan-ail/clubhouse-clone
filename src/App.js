55603ebddcf3578c265645e91101821c456c2683
Darshan Ail <darsh1076@gmail.com>
2022-03-26 16:02:57 +0530

    added code confirmation page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PlanLayout from './pages/Layouts/PlanLayout';
 import Welcome from './pages/Welcome';
 import { Route, Routes } from "react-router-dom";
 import PhoneConfirmation from './pages/PhoneConfirmation';
+import CodeConfirm from './pages/CodeConfirm';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Welcome />} />
         <Route exact path='/invite' element={<PhoneConfirmation />} />
+        <Route exact path='/code_confirm' element={<CodeConfirm />} />
       </Routes>
     </PlanLayout>
   );
