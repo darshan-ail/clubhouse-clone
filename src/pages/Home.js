58c1d981eca6fa244e79196577b1ddee34678335
Darshan Ail <darsh1076@gmail.com>
2022-03-28 11:48:12 +0530

    Start a room button added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,8 @@ import Header from "../component/Header";
 import AppLayout from "./Layouts/AppLayout";
 import style from "../style/home.module.css"
 import RoomInforCard from "../component/RoomInforCard";
+import { AiOutlinePlus } from 'react-icons/ai'
+import { BsGrid3X3Gap } from "react-icons/bs"
 
 const Home = props => {
     return (
@@ -12,6 +14,15 @@ const Home = props => {
             <div className={style.homeContainer}>
                 <DailyInfoCard />
                 <RoomInforCard />
+            </div>
+            <div className={style.actionBtn}>
+                <button>
+                    <AiOutlinePlus className="mr-2" />
+                    Start a room
+                </button>
+                <button>
+                    <BsGrid3X3Gap />
+                </button>
             </div>
         </AppLayout>
     )
